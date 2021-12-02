1f9df09e43f132c22d09bc997664b57bd22a9664
Marcus Ho <marcus.ho@oocl.com>
2021-12-02 13:59:04 +0800

    [TTD practice 1][Marcus]feat:  Whizz Case.

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,7 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
 
     public String countOff(int order) {
 
@@ -10,6 +11,8 @@ public class FizzBuzz {
             return FIZZ;
         } else if (order % 5 == 0) {
             return BUZZ;
+        } else if (order % 7 == 0) {
+            return WHIZZ;
         }
         return String.valueOf(order);
     }
