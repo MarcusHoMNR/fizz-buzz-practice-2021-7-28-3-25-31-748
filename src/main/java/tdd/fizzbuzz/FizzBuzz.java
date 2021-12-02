2e3b3821f848d58e7587fa4ad601698e822c842f
Marcus Ho <marcus.ho@oocl.com>
2021-12-02 14:08:05 +0800

    [TTD practice 1][Marcus]feat:   FizzBuzzWhizz Case.

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -9,6 +9,9 @@ public class FizzBuzz {
 
         if (order % 3 == 0) {
             if (order % 5 == 0) {
+                if (order % 7 == 0) {
+                    return FIZZ + BUZZ + WHIZZ;
+                }
                 return FIZZ + BUZZ;
             }
             if (order % 7 == 0) {
