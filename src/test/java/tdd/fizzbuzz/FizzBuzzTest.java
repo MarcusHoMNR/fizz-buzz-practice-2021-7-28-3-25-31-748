47b582112b147488ab410c215c2743f406241352
Marcus Ho <marcus.ho@oocl.com>
2021-12-02 13:52:55 +0800

    [TTD practice 1][Marcus]feat: TestCase Normal Case.

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_order_when_count_off_given_order_number_with_normal_case() {
+        //given
+        int order = 4;
+        String expectedResult = "4";
+
+        // when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.countOff(order);
+
+        //then
+        assertEquals(expectedResult, result);
+    }
 }
